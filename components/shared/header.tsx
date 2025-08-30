6f274cb8e95afc31e3fe08f2ef6ee141d79aeebe
rapward <edward.uramaev@gmail.com>
2025-08-30 18:27:07 +0300

    first

---
+++ components/shared/header.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Container } from "./container";
+import Image from 'next/image'
+import { Button } from "../ui";
+import { ShoppingCart, Star, User } from "lucide-react";
+
+interface Props {
+    className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+    return (
+        <header className={cn('border border-b', className)}>
+            <Container className='flex items-center justify-between py-8'>
+                {/* Левая часть */}
+                <div className="flex items-center gap-4">
+                    <Image src="/next.svg" alt="Logo" width={85} height={85} />
+                    <div>
+                        <h1 className="text-2xl uppercase font-black">Fortvin text</h1>
+                        <p className="text-sm text-gray-400 leading-3">стройте вместе с нами</p>
+                    </div>
+                </div>
+                {/* Правая часть */}
+                <div className="flex items-center gap-3">
+                    <Button variant="outline"><Star size={16}/></Button>
+                    <Button variant="outline"><ShoppingCart size={16}/></Button>
+                    <Button variant="outline"><User size={16}/>Войти</Button>
+                </div>
+            </Container>
+        </header>
+    )
+}
