55b0382d2a9e43dbbb43d529a6306de71e46feb3
Evgeniya Mishchenko <evgeniyalike@yandex.ru>
2025-10-03 02:06:50 +0300

    Profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -2,6 +2,7 @@ import { ProfileForm } from '@/components/shared';
 import LogoutButton from '@/components/shared/profile/LogoutButton';
 import { getUserSession } from '@/lib/get-user-session';
 import { prisma } from '@/prisma/prisma-client';
+import Link from "next/link";
 
 import { redirect } from 'next/navigation';
 
@@ -11,6 +12,7 @@ type OrderItem = {
     productItem: {
       price: number;
       product: {
+        id: number;
         name: string;
         size?: string;
         color?: string;
@@ -28,24 +30,12 @@ export default async function ProfilePage() {
     return redirect('/not-auth');
   }
 
-  // const user = await prisma.user.findFirst({ 
-  //   where: 
-  //     { id: Number(session?.id) }, 
-  //   select: {
-  //       fullName: true,
-  //       email: true,
-  //       orders: true,
-  //     },
-  //   });
-
-
-
-
   const user = await prisma.user.findFirst({ 
   where: { id: Number(session?.id) }, 
   select: {
     fullName: true,
     email: true,
+    phone: true,
     orders: {
       select: {
         id: true,
@@ -85,17 +75,17 @@ export default async function ProfilePage() {
                                 <p className="small-bold-text text-[var(--color-blue)]">ФИО</p>
                                 <h3>{user.fullName}</h3>
                             </div>
-                            {/* <div className="blue-card gap-[5px]">
+                            <div className="blue-card gap-[5px]">
                                 <p className="small-bold-text text-[var(--color-blue)]">Номер телефона</p>
-                                <h3>{profile.phone}</h3>
-                            </div> */}
+                                <h3>{user.phone ?? 'Телефон не указан'}</h3>
+                            </div>
                             <div className="blue-card gap-[5px]">
                                 <p className="small-bold-text text-[var(--color-blue)]">Email</p>
                                 <h3>{user.email}</h3>
                             </div>
                         </div>
                         <div className="flex flex-row gap-[10px]">
-                            <button className="small-button pl-[20px] pr-[20px] pt-[8px] pb-[8px] w-auto">Редактировать</button>
+                            {/* <button className="small-button pl-[20px] pr-[20px] pt-[8px] pb-[8px] w-auto">Редактировать</button> */}
                             <LogoutButton />
                         </div>
                   </div>
@@ -125,27 +115,45 @@ export default async function ProfilePage() {
 
                       <div className="flex flex-col gap-[5px]">
                         <p className="small-bold-text text-[var(--color-blue)]">Состав заявки</p>
-                        <ul className="list-disc list-inside">
-                          {orderItems.map((item, idx) => (
-                            <li key={idx} className="flex flex-row gap-2 items-center">
-                              {item.productItem.product.imageUrl && (
-                                <img
-                                  src={item.productItem.product.imageUrl}
-                                  alt={item.productItem.product.name}
-                                  className="w-10 h-10 object-cover rounded"
-                                />
-                              )}
-                              <span>
-                                {item.productItem.product.name} — {item.quantity} шт —{' '}
-                                {(item.productItem.product.quantityPerPallet ?? 0) * (item.productItem.product.retailPriceRubWithVAT ?? 0) * item.quantity} ₽
-                              </span>
-                              <div className="text-sm text-gray-600">
-                                В поддоне: {item.productItem.product.quantityPerPallet} шт, РРЦ: {item.productItem.product.retailPriceRubWithVAT} ₽
-                              </div>
-
-                            </li>
-                          ))}
-                        </ul>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-[960px]">
+                          <div>
+                            {orderItems.map((item, idx) => (
+                              <Link key={idx} href={`/product/${item.productItem.product.id}`}>
+                                <div className="flex flex-row gap-2 items-center bg-[var(--background)] border border-black p-[15px]">
+                                <div>
+                                  {item.productItem.product.imageUrl && (
+                                    <img
+                                      src={item.productItem.product.imageUrl}
+                                      alt={item.productItem.product.name}
+                                      className="w-[96px] h-[96px] object-cover rounded"
+                                    />
+                                  )}
+                                  </div>
+                                  {/* <div className='flex flex-col w-full'>
+                                      <p>{item.productItem.product.name}</p>
+                                      <div className='blex flex-row w-full justify-between'>
+                                        <p className='font-bold'> {(item.productItem.product.quantityPerPallet ?? 0) * (item.productItem.product.retailPriceRubWithVAT ?? 0) * item.quantity} ₽</p>
+                                        <p>В поддоне: {item.productItem.product.quantityPerPallet} шт, РРЦ: {item.productItem.product.retailPriceRubWithVAT} ₽</p>
+                                      </div>
+                                  </div> */}
+                                  <div className="flex flex-col h-[96px] justify-between pb-2 pt-2 w-full">
+                                    <h6 className="text-sm font-medium text-gray-800 mb-2 leading-tight">
+                                      {item.productItem.product.name}
+                                    </h6>
+                                    <div className="flex justify-between items-center">
+                                      <span className="text-lg font-bold text-gray-800">
+                                        {(item.productItem.product.quantityPerPallet ?? 0) * (item.productItem.product.retailPriceRubWithVAT ?? 0) * item.quantity} ₽
+                                      </span>
+                                      <span className="small-text text-gray-600">
+                                        В поддоне: {item.productItem.product.quantityPerPallet} шт, РРЦ: {item.productItem.product.retailPriceRubWithVAT} ₽
+                                      </span>
+                                  </div>
+                                </div>
+                                </div>
+                              </Link>
+                            ))}
+                          </div>
+                        </div>
                       </div>
 
                       <div className="flex flex-col gap-[5px]">
