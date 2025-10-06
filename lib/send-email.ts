dd8da1546b5ff032ea6043ae6b161f5d30ae1e07
Ohrana911 <edward.uramaev@gmail.com>
2025-10-06 03:57:49 +0300

    правки в send-email.ts

--- lib/send-email.ts
+++ lib/send-email.ts
@@ -4,7 +4,8 @@ export const sendEmail = async (to: string, subject: string, template: React.Rea
   const resend = new Resend(process.env.RESEND_API_KEY);
 
   const { data, error } = await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'plitka@aucmos.store',
+    // from: 'Fortvin@plitka.ru',
     to,
     subject,
     text: '',
