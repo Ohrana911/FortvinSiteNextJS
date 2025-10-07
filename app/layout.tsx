3f1c9de6bf0a4e0de9c6d51653c718317edeaadb
Ohrana911 <edward.uramaev@gmail.com>
2025-10-07 19:35:20 +0300

    Убрал все css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import './articles/articles.css'; // ✅ оставить файл на месте
+// import "./globals.css";
+// import './articles/articles.css'; // ✅ оставить файл на месте
 import { Header } from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
 import { useEffect, useState } from "react";
