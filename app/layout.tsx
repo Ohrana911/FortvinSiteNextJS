1f62adef236d3ada5922a476ef4831a2ec0a6189
mirmad <mirmadar@mail.ru>
2025-10-08 02:37:58 +0300

    Импорт в layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 // import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
+import "./globals.css";
 // import './articles/articles.css'; // ✅ оставить файл на месте
 import { Header } from "@/components/shared/header";
 // import Footer from "@/components/shared/footer";
