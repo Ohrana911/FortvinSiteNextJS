5585a4ff858352f5f4b870fd9d3e618f32118156
Ohrana911 <edward.uramaev@gmail.com>
2025-10-07 19:31:00 +0300

    Убрал services.css

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import "./services.css";
+// import "./services.css";
 import { Button } from "@/components/ui";
 import RequestForm from "@/components/shared/request-form";
 
