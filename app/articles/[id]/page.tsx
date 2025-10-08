d1afe15cc2a786ed8895382011e6fe970e22758b
mirmad <mirmadar@mail.ru>
2025-10-08 03:46:23 +0300

    Вкл стили для услуг и постов

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,6 +1,6 @@
 // import { Button } from '@/components/ui';
 // import articlesData from '../../../data/articles.json';
-// import '../articles.css';
+import '../articles.css';
 // import Link from 'next/link';
 // import React from 'react';
 
