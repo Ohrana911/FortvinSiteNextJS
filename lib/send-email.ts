3479e593c77a559e0149fc62b5ed53e50c33f5ec
Ohrana911 <edward.uramaev@gmail.com>
2025-10-14 08:29:52 +0300

    Отправка письма от fortvin домена

--- lib/send-email.ts
+++ lib/send-email.ts
@@ -4,8 +4,7 @@ export const sendEmail = async (to: string, subject: string, template: React.Rea
   const resend = new Resend(process.env.RESEND_API_KEY);
 
   const { data, error } = await resend.emails.send({
-    from: 'plitka@aucmos.store',
-    // from: 'Fortvin@plitka.ru',
+    from: 'plitka@fortvin',
     to,
     subject,
     text: '',
